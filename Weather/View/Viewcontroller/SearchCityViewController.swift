15a92327a186d7588cdc038ee868ef75c5db5d14
hessam Mahdiabadi <h.mahdiabadi@gmail.com>
2020-02-22 20:02:03 +0330

    add view controller

---
+++ Weather/View/Viewcontroller/SearchCityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchCityViewController.swift
+//  Weather
+//
+//  Created by Hessam Mahdiabadi on 2/22/20.
+//  Copyright © 2020 Hessam Mahdiabadi. All rights reserved.
+//
+
+import UIKit
+
+class SearchCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather/View/Viewcontroller/WeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherViewController.swift
+//  Weather
+//
+//  Created by Hessam Mahdiabadi on 2/22/20.
+//  Copyright © 2020 Hessam Mahdiabadi. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
